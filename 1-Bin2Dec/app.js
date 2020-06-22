b25259c408c059d0a0ab0912915d7367d3e5c0cc
DanielaHA <dhiguitaa17@gmail.com>
2020-06-22 12:42:07 -0500

    se termino la logica del programa

--- 1-Bin2Dec/app.js
+++ 1-Bin2Dec/app.js
@@ -2,31 +2,46 @@
 
 const entrada = document.querySelector("#entrada");
 const btnEnviar = document.querySelector(".btn-enviar");
+const mensajes = document.querySelector(".mensajes");
+const resutlado = document.querySelector(".resultado");
 // event listeners
 
 entrada.addEventListener("input", (e) => {
-	if (e.data !== 0 || e.data !== 1) {
-		console.log("solo 0 y 1");
-		// mensaje()
-    }
-    if (entrada.value.length > 8) {
-        console.log('solo 8 digitos')
-    }
+	if (e.inputType == "deleteContentBackward") {
+		quitarMensaje();
+	} else if (e.data != 0 && e.data != 1 && e.data != null) {
+		mostrarMensaje("solo 0s y 1s");
+	}
+	if (entrada.value.length > 8) {
+		console.log("solo 8 digitos");
+	}
 });
 
 btnEnviar.addEventListener("click", (e) => {
 	e.preventDefault();
-    const numero = entrada.value;
-    if (typeof(numero) === 'string'){
-        console.log("holi");
-        //mensaje()
-    }
+	// pasamos el numero a
+	const numero = entrada.value;
+	if (isNaN(parseInt(numero))) {
+		mostrarMensaje("solo 0s y 1s");
+	} else {
+		let binario = 0;
+		for (let i = 0; i < numero.length; i++) {
+			let digito = numero.slice(i, i + 1);
+			const exponente = numero.length - i - 1;
+			binario += digito * 2 ** exponente;
+		}
+		mostrarResultado(binario);
+	}
 });
 
 // funciones
-function mensaje() {
-	const msj = "Ingresar solo 0s y 1s";
-	let nodo = document.createElement("h3");
-	nodo.textContent = msj;
-	entrada.insertBefore(nodo, entrada);
+function mostrarMensaje(msj) {
+	mensajes.textContent = msj;
+}
+function quitarMensaje() {
+	mensajes.textContent = "";
+}
+
+function mostrarResultado(binario) {
+	resutlado.textContent = `Decimal: ${binario}`;
 }
