518e4311642832924df3ca8608fd280eeeb5d3f9
DanielaHA <dhiguitaa17@gmail.com>
2020-06-22 15:30:48 -0500

    se lee la información de los input
    se lee la info y estan los event pero el copiar hay que mejorarlo para
    que pueda tener los 4 valores de una.

--- 2-borderPreview/app.js
+++ 2-borderPreview/app.js
@@ -1,28 +1,42 @@
 // variables
-const caja = document.querySelector('.caja')
-const inputs = document.querySelectorAll('input')
+const caja = document.querySelector(".caja");
+const inputs = document.querySelectorAll("input");
+const btnCopy = document.querySelector(".btn-copy");
 // event listeners
-inputs.forEach((input)=>{
-    input.addEventListener('keydown',leerEntrada)
-})
-console.log(inputs)
+inputs.forEach((input) => {
+	input.addEventListener("keyup", leerEntrada);
+});
+btnCopy.addEventListener("click", copiar);
 
 // funciones
-function leerEntrada(e){
-    const id = e.currentTarget.id
-    if(id === 'left-top'){
-        caja.style.border
-    }
-    if(id === 'left-bottom'){
-        console.log('2')
-        
-    }
-    if(id === 'right-top'){
-        console.log('3')
-        
-    }
-    if(id === 'right-bottom'){
-        console.log('4')
-
-    }
+function leerEntrada(e) {
+	const input = e.currentTarget;
+	const id = input.id;
+	if (id === "left-top") {
+		caja.style.borderTopLeftRadius = `${input.value}px`;
+	}
+	if (id === "left-bottom") {
+		caja.style.borderBottomLeftRadius = `${input.value}px`;
+	}
+	if (id === "right-top") {
+		caja.style.borderTopRightRadius = `${input.value}px`;
+	}
+	if (id === "right-bottom") {
+		caja.style.borderBottomRightRadius = `${input.value}px`;
+	}
 }
+
+function copiar(e) {
+	e.preventDefault();
+	let copia = "";
+	inputs.forEach((input) => {
+		// texto que se va a copiar
+		console.log(input);
+		// se selecciona lo que se va a copiar
+		input.select();
+		// se copia
+		console.log(document.execCommand("copy"));
+	});
+	console.log(copia);
+}
+
