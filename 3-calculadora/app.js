42aee27aea5f5f3be9960b45389e9998b3f4ee58
DanielaHA <dhiguitaa17@gmail.com>
2020-06-23 10:37:04 -0500

    limite de 8 digitos y mensaje al pasrlo

--- 3-calculadora/app.js
+++ 3-calculadora/app.js
@@ -10,5 +10,16 @@ function mostrarNumero(e) {
     e.preventDefault();
     const valor = e.currentTarget.innerHTML
     let  numPantalla = pantalla.textContent
-    pantalla.textContent = numPantalla + valor
+    if (numPantalla.length < 8) {
+        pantalla.textContent = numPantalla + valor
+    }else{
+        const container = document.querySelector('.container')
+        const contenido = document.querySelector('.contenido')
+        let msj = document.createElement('h3')
+        msj.textContent = 'maximo 8 digitos'
+        container.insertBefore(msj,contenido)
+        setTimeout(()=>{
+            document.querySelector('h3').remove();
+        },1000)
+    }
 }
