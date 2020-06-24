ac250962ffc739a7941c215ef01b38f8ee707e59
DanielaHA <dhiguitaa17@gmail.com>
2020-06-24 08:58:17 -0500

    cambian y se pausan las luces al dar click
    cuando las luces estan detenidas al dar click las luces empiezan a
    funcionar, si estan funcionando al dar click se detienen

--- 4-chrismasLights/app.js
+++ 4-chrismasLights/app.js
@@ -2,6 +2,8 @@
 // ---------
 const btnInicio = document.querySelector(".btn");
 const luces = document.querySelectorAll(".luz");
+let intervalo = 0
+let time = 1000
 // event listeners
 // ---------------
 btnInicio.addEventListener("click", inicio);
@@ -9,14 +11,19 @@ btnInicio.addEventListener("click", inicio);
 // ---------
 function inicio(e){
     e.preventDefault()
-    const intervalo = 1000
-    setInterval(restaurar, intervalo);
+    if(e.currentTarget.classList.contains('inicio')){
+        e.currentTarget.classList.remove('inicio')
+        intervalo = setInterval(restaurar, time);
+    }else{
+        clearInterval(intervalo)
+        e.currentTarget.classList.add('inicio')
+    }
+
 }
 
 function restaurar() {
     for (let i = 0; i < luces.length; i++) {
         if(i%2 === 0){
-            console.log(luces[i])
             luces[i].classList.toggle('verde')
         }else{
             luces[i].classList.toggle('rojo')
