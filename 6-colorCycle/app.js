4ab87b1c1e3730c3a6273f33afcdcb9118a5aca1
DanielaHA <dhiguitaa17@gmail.com>
2020-06-26 09:37:17 -0500

    obteniendo el color desde el input
    al hacer click sobre el boton, se cambia el color de la caja por el que
    el usuario pasa como color base

--- 6-colorCycle/app.js
+++ 6-colorCycle/app.js
@@ -2,22 +2,36 @@
 // ---------
 const inicial = document.querySelector('#inicial')
 const incremento = document.querySelector('#incremento')
+const btnControl = document.querySelector('.btn-control')
+const cajaColor = document.querySelector('.color')
 // event listeners
 // ---------------
-inicial.addEventListener('keyup',valorInicial)
+
+btnControl.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if (btnControl.classList.contains('inicio')) {
+        valorInicial()
+    }
+})
 // funciones
 // ---------
 
 function valorInicial(){
     let hexa = inicial.value
     if (hexa.length === 6) {
+        // se sacan los numeros en hexadecimal para cada color
         let red = hexa.slice(0,2)
         let green = hexa.slice(2,4)
         let blue = hexa.slice(4,6)
-        console.log(red,green,blue)
-        
+        // se pasan a decimal
+        red = parseInt(red,16)
+        green = parseInt(green,16)
+        blue = parseInt(blue,16)
+        cambioColor(red,green,blue)
     }
 
 }
 
-
+function cambioColor(red,green,blue){
+    cajaColor.style.backgroundColor = `rgb(${red},${green},${blue})`
+}
