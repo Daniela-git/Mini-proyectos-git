516f25a505bb42c0f2e24a33423f5247815c01db
DanielaHA <dhiguitaa17@gmail.com>
2020-06-26 09:59:59 -0500

    funciona el cambio de color
    al dar click el color va aumentando hasta llegar a blanco, pero de
    momento la suma no para al llegar a blanco solo que el rgb ya no
    reconoce más.
    de momento los 3 colores aumentan la misma cantidad.

--- 6-colorCycle/app.js
+++ 6-colorCycle/app.js
@@ -1,37 +1,49 @@
 // variables
 // ---------
-const inicial = document.querySelector('#inicial')
-const incremento = document.querySelector('#incremento')
-const btnControl = document.querySelector('.btn-control')
-const cajaColor = document.querySelector('.color')
+const inicial = document.querySelector("#inicial");
+const incremento = document.querySelector("#incremento");
+const btnControl = document.querySelector(".btn-control");
+const cajaColor = document.querySelector(".color");
+
+let red = 0
+let green = 0
+let blue = 0
+let intervalo = 0;
+let inc = parseInt(incremento.value)
 // event listeners
 // ---------------
 
-btnControl.addEventListener('click',(e)=>{
-    e.preventDefault()
-    if (btnControl.classList.contains('inicio')) {
-        valorInicial()
-    }
-})
+btnControl.addEventListener("click", (e) => {
+	e.preventDefault();
+	if (btnControl.classList.contains("inicio")) {
+		valorInicial();
+	}
+});
 // funciones
 // ---------
 
-function valorInicial(){
-    let hexa = inicial.value
-    if (hexa.length === 6) {
-        // se sacan los numeros en hexadecimal para cada color
-        let red = hexa.slice(0,2)
-        let green = hexa.slice(2,4)
-        let blue = hexa.slice(4,6)
-        // se pasan a decimal
-        red = parseInt(red,16)
-        green = parseInt(green,16)
-        blue = parseInt(blue,16)
-        cambioColor(red,green,blue)
-    }
-
+function valorInicial() {
+	let hexa = inicial.value;
+	if (hexa.length === 6) {
+		// se sacan los numeros en hexadecimal para cada color
+		red = hexa.slice(0, 2);
+		green = hexa.slice(2, 4);
+		blue = hexa.slice(4, 6);
+		// se pasan a decimal
+		red = parseInt(red, 16);
+		green = parseInt(green, 16);
+		blue = parseInt(blue, 16);
+		// se pone la caja del color base
+		cajaColor.style.backgroundColor = `rgb(${red},${green},${blue})`;
+		intervalo = setInterval(cambioColor, 500);
+	}
 }
 
-function cambioColor(red,green,blue){
-    cajaColor.style.backgroundColor = `rgb(${red},${green},${blue})`
+function cambioColor() {
+    
+	red = red + inc;
+	green = green + inc;
+	blue = blue + inc;
+    console.log(red, green, blue)
+	cajaColor.style.backgroundColor = `rgb(${red},${green},${blue})`;
 }
