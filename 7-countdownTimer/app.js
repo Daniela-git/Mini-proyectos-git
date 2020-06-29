23d647bd571821c9e8787e60f0a6215cf77ab96c
DanielaHA <dhiguitaa17@gmail.com>
2020-06-29 11:49:51 -0500

    se tienen los datos que ingresan
    se obtienen los datos que el usuario ingresa, la fecha y la hora.

--- 7-countdownTimer/app.js
+++ 7-countdownTimer/app.js
@@ -1,2 +1,28 @@
 // <input type="date" id="datemin" name="datemin" min="2000-01-02"><br><br>
 // este atributo de min puede ser util para las fechas ponerlo por js es lo más logico
+
+// VARIABLES
+const datos = document.querySelector('#datos')
+const numeros = document.querySelectorAll('.numero')
+
+// EVENT LISTENERS
+datos.addEventListener('submit',obtenerDatos)
+
+
+// FUNCIONES    
+function obtenerDatos(e){
+    e.preventDefault()
+    //tomamos lo que necesitamos del form
+    let fecha = e.target.children[1].children[1].value
+    let hora = e.target.children[2].children[1].value
+    fecha = fecha.split('-')
+    hora = hora.split(':')
+    iniciarContador(fecha, hora)
+
+}
+
+function iniciarContador(fecha, hora){
+    // año mes dia = fecha
+    console.log(hora)
+
+}
