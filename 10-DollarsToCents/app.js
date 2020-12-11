fd4802ecaa243fc3fc9f9f3a7101ce7d85c9578e
Daniela <dhiguitaa17@gmail.com>
2020-12-11 11:16:32 -0500

    correccion en la lectura del valor

--- 10-DollarsToCents/app.js
+++ 10-DollarsToCents/app.js
@@ -15,18 +15,18 @@ send.addEventListener("click", readData);
 //functions
 function readData(e) {
 	e.preventDefault;
-	//read de input and past to integer
-	let dollar = input.value.replace(".", "");
-	dollar = parseInt(dollar);
+  //read de input and past to integer
+  
+	let dollar = parseFloat(input.value)*100;
 	toCoins(dollar);
 }
 
 function toCoins(dollar) {
 	let dic = {
-		1: { name: "penny", number: 0 },
-		5: { name: "nickel", number: 0 },
-		10: { name: "dime", number: 0 },
-		25: { name: "quarter", number: 0 },
+		1: { name: "penny 1¢", number: 0 },
+		5: { name: "nickel 5¢", number: 0 },
+		10: { name: "dime 10¢", number: 0 },
+		25: { name: "quarter 25¢", number: 0 },
 	};
 	let list = [25, 10, 5, 1];
 	const reducer = (acc, curr) => {
