0ed07e7142be2b9488bb73d8a7d7bf2057f6effa
Daniela <dhiguitaa17@gmail.com>
2020-12-11 11:11:38 -0500

    se crea el html para mostrar el resultado

--- 10-DollarsToCents/app.js
+++ 10-DollarsToCents/app.js
@@ -14,33 +14,48 @@ send.addEventListener("click", readData);
 
 //functions
 function readData(e) {
-  e.preventDefault;
-  //read de input and past to integer
-  let dollar = input.value.replace(".","");
-  dollar = parseInt(dollar)
-  toCoins(dollar)
+	e.preventDefault;
+	//read de input and past to integer
+	let dollar = input.value.replace(".", "");
+	dollar = parseInt(dollar);
+	toCoins(dollar);
 }
 
-function toCoins(dollar){
-  let dic = {
-    1:{name:"penny",number:0},
-    5:{name:"nickel",number:0},
-    10:{name:"dime",number:0},
-    25:{name:"quarter",number:0}
-  }
-  let list=[25,10,5,1]
-
-  const reducer = (acc,curr) =>{
-    if (acc>=curr){
-      dic[curr].number = Math.trunc(acc/curr)
-      return acc-Math.trunc(acc/curr)*curr
-    }else{      
-      return acc
-    }
-
-  }
-  let total = list.reduce(reducer,dollar)
-  console.log(dic)
+function toCoins(dollar) {
+	let dic = {
+		1: { name: "penny", number: 0 },
+		5: { name: "nickel", number: 0 },
+		10: { name: "dime", number: 0 },
+		25: { name: "quarter", number: 0 },
+	};
+	let list = [25, 10, 5, 1];
+	const reducer = (acc, curr) => {
+		if (acc >= curr) {
+			dic[curr].number = Math.trunc(acc / curr);
+			return acc - Math.trunc(acc / curr) * curr;
+		} else {
+			return acc;
+		}
+	};
+  list.reduce(reducer, dollar);
+  showResults(dic)
   
+}
+
+function showResults(dic){
+  let results = document.querySelector(".results");
+  for(const[key, coin] of Object.entries(dic)) {
+    let divResult = document.createElement("div")
+    let title = document.createElement("h3")
+    let quantity = document.createElement("h4")
+    title.innerText = coin.name
+    quantity.innerText = coin.number
+    divResult.appendChild(title)
+    divResult.appendChild(quantity)
+    divResult.classList.add("result")
+    title.classList.add("title_result")
+    quantity.classList.add("quantity_result")
+    results.appendChild(divResult)
+  }
 
 }
